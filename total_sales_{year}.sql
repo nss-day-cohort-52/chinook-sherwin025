6f5660f7e92f92a8faabefbec3a4887e47cb2730
Sherwin Vargas <svargas025@gmail.com>
2022-02-21 11:37:34 -0600

    exercise (9 real)

---
+++ total_sales_{year}.sql
@@ -0,0 +1,6 @@
+Select 
+    Sum(Total) as Yearly_total,
+    strftime('%Y', InvoiceDate) as "Year"
+From Invoice
+WHERE InvoiceDate like "2009%" OR InvoiceDate like "2011%"
+Group By InvoiceDate like "%2009%"
